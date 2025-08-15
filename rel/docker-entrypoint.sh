dddc185f3433a2b3db677c237c721996cbd9fc92
merchmind <224538810+merchmind@users.noreply.github.com>
2025-08-15 16:51:45 +0200

    Update docker-entrypoint.sh

--- rel/docker-entrypoint.sh
+++ rel/docker-entrypoint.sh
@@ -2,7 +2,11 @@
 set -e
 
 if [ "$1" = 'run' ]; then
+      echo "🚀 Running database migrations..."
+      /app/bin/plausible eval "Plausible.Release.migrate"
+      echo "✅ Starting Plausible..."
       exec /app/bin/plausible start
+
 
 elif [ "$1" = 'db' ]; then
       exec /app/"$2".sh

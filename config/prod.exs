20d5ae60038a0b306987dacdfd8c019bfddd2c66
merchmind <224538810+merchmind@users.noreply.github.com>
2025-08-15 14:41:13 +0200

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -5,3 +5,8 @@ config :plausible, PlausibleWeb.Endpoint,
   check_origin: false,
   server: true,
   code_reloader: false
+
+config :plausible, Plausible.Repo,
+  pool_size: 20,
+  queue_target: 10000,
+  queue_interval: 1000

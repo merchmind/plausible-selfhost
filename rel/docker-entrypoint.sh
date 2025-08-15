f0e66595686e056d18fff4ff7fad582fa3a0efab
merchmind <224538810+merchmind@users.noreply.github.com>
2025-08-15 18:52:32 +0200

    Update docker-entrypoint.sh

--- rel/docker-entrypoint.sh
+++ rel/docker-entrypoint.sh
@@ -2,27 +2,25 @@
 set -e
 
 if [ "$1" = 'run' ]; then
--     exec /app/bin/plausible start
-+     echo "🚀 Ensuring applications are started…"
-+     # start Ecto and its Repo so we can migrate
-+     /app/bin/plausible eval "\
-+       Application.ensure_all_started(:postgrex); \
-+       Application.ensure_all_started(:ecto_sql); \
-+       Application.ensure_all_started(:plausible); \
-+       Ecto.Migrator.run(\
-+         Plausible.Repo, \
-+         Application.app_dir(:plausible, \"priv/repo/migrations\"), \
-+         :up, \
-+         all: true\
-+       )\
-+     "
-+     echo "✅ Migrations finished, starting web server…"
-+     exec /app/bin/plausible start
+    echo "🚀 Ensuring applications are started…"
+    # Run all Ecto migrations before we boot the web server:
+    /app/bin/plausible eval \
+      "Application.ensure_all_started(:postgrex); \
+       Application.ensure_all_started(:ecto_sql); \
+       Application.ensure_all_started(:plausible); \
+       Ecto.Migrator.run(\
+         Plausible.Repo, \
+         Application.app_dir(:plausible, \"priv/repo/migrations\"), \
+         :up, \
+         all: true\
+       )"
+    echo "✅ Migrations finished, starting web server…"
+    exec /app/bin/plausible start
 
 elif [ "$1" = 'db' ]; then
-     exec /app/"$2".sh
+    exec /app/"$2".sh
 else
-     exec "$@"
+    exec "$@"
 fi
 
 exec "$@"
